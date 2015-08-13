c588d9f2d189b2f6ed08f0cfa4c77a9b0b362cd9
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-08-12 21:57:14 -0500

    First commit for 'Pragmatic Studio's: Ruby Programming' project

---
+++ studio_game.rb
@@ -0,0 +1,12 @@
+name1 = "larry"
+name2 = "curly"
+name3 = "moe"
+health1 = 60
+health2 = 60
+health3 = 60
+
+puts "#{stooge1}'s health is #{health1}"
+
+puts "Players:\n\tlarry\n\tcurly\n\tmoe"
+
+puts "Players:\n\t#{stooge1}\n\t#{stooge2}\n\t#{stooge3}"
