914e1ad9dea73a6584381707b179e3ca7c3ffb9e
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-11-28 17:40:00 -0600

    Dry out code and stop output when tests run

--- player_spec.rb
+++ player_spec.rb
@@ -3,19 +3,37 @@ require_relative 'player'
 
 describe 'Player' do
 
-  it 'has a capitalized name' do
-    player = Player.new("larry", 150)
-
-    player.name.should == "Larry"
+  before do
+    $stdout = StringIO.new
+    @initial_health = 150
+    @player = Player.new("larry", 150)
   end
 
-  it "has an initial health"
+  it 'has a capitalized name' do
+    @player.name.should == "Larry"
+  end
 
-  it "has a string representation"
+  it "has an initial health" do
+    @player.health.should == 150
+  end
 
-  it "computes a score as the sum of its health and length of name"
+  it "has a string representation" do
+    @player.to_s.should == "I'm Larry with a health of 150 and a score of 155."
+  end
 
-  it "increases health by 15 when w00ted"
+  it "computes a score as the sum of its health and length of name" do
+    @player.score.should == (150 + 5)
+  end
 
-  it "decreases health by 10 when blammed"
+  it "increases health by 15 when w00ted" do
+    @player.w00t
+
+    @player.health.should == @initial_health + 15
+  end
+
+  it "decreases health by 10 when blammed" do
+    @player.blam
+
+    @player.health.should == @initial_health - 10
+  end
 end
